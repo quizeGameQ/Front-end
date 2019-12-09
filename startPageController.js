7549168df06f1e3b565220bc02e9f508f2639b1a
AbdulrahmanYasser95 <abdulrahmanyasser@live.com>
2019-12-09 02:47:19 +0200

    8/12/19

--- startPageController.js
+++ startPageController.js
@@ -1,14 +1,26 @@
 var starPage = angular.module('startPage', []);
 
-starPage.controller('startPageController', ['$scope', startPageController]);
+starPage.controller('startPageController', ['$scope', '$compile', startPageController]);
 
-function startPageController($scope) {
+function startPageController($scope, $compile) {
     $.getJSON("levels.json", function (data) {
         $scope.levels = data;
     });
 
     $scope.showLevels = function () {
-        console.log($scope.levels.levels);
-        $('#subTitle').html("<div>" + $scope.levels.levels + "</div>");
+        var levels = "<div class='col-md-6 text-center'"
+            + " ng-repeat='level in levels.levels'>"
+            + "<p class='levelBlock'>{{level}}</p>"
+            + "</div>";
+        $('#subTitle').html(
+            $compile(levels)($scope)
+        );
+        $('.levelBlock').click(function () {
+            console.log('levelBlock clicked');
+        });
+    }
+
+    $scope.goToLevel = function (source) {
+        console.log(source)
     }
 }
